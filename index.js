2484a578a4905bfa1b3ef9b02d01403d5b65ec9c
pennymourt69-eng <pennymourt69@gmail.com>
2026-04-06 11:55:08 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys";
+import pino from "pino";
+
+async function connectToWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: true,
+        logger: pino({ level: "silent" }),
+    });
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection } = update;
+        if (connection === 'open') {
+            console.log('{"msg":"connected to WA"}');
+        }
+    });
+
+    sock.ev.on('creds.update', saveCreds);
+}
+
+connectToWhatsApp();
